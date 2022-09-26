4ed8421d2302d5c1df2713f377b7fa8265a5c9dc
zhuangjh123 <114286218+zhuangjh123@users.noreply.github.com>
2022-09-26 10:44:43 +0800

    pull

--- src/main/java/com/git/GITTEST.java
+++ src/main/java/com/git/GITTEST.java
@@ -9,5 +9,6 @@ public class GITTEST {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
